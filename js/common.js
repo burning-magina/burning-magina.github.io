3ae3dbcf8f5d22294e307add19f63bde1582b305
bail <yuanquan.ni@qq.com>
2017-11-03 22:23:25 +0800

    注释语句

---
+++ js/common.js
@@ -0,0 +1,24 @@
+/*执行事件*/
+$(function () {
+
+    // 打字机效果
+    var str = '你好!\n';
+    str += 'nice to meet you!\n';
+    str += '我是只懒猫~\n';
+    str += '有时候也是小白鼠- -\n';
+	str += '你的情谊，我不会拒绝(ˇ3ˇ)\n';
+	str += '我也愿意陪伴在你身边~\n';
+    str += '欢迎你!';
+    Printer.init(str, {
+        selector: 'text_typewriter'
+    }).print();
+
+    setTimeout(function() {
+        _init();
+    }, str.length * 100);
+	 function _init(){
+        //隐藏loading页面
+        $('.loading_typewriter').fadeOut(5000);
+		window.location="my_page.html";
+	 }
+});
