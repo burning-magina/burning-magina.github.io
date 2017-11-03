895c2972b1b84cd7bf505cc8384d955ebcf53cd0
bail <yuanquan.ni@qq.com>
2017-11-04 00:09:22 +0800

    --

--- js/common.js
+++ js/common.js
@@ -2,13 +2,15 @@
 $(function () {
 
     // 打字机效果
-    var str = '你好!\n';
+  var str = '你好!\n';
     str += 'nice to meet you!\n';
     str += '我是只懒猫~\n';
     str += '有时候也是小白鼠- -\n';
-	str += '你的情谊，我不会拒绝(ˇ3ˇ)\n';
+	str += '你的情谊，我不会拒绝\n';
+	str += '--------(ˇ3ˇ)-------\n';
 	str += '我也愿意陪伴在你身边~\n';
-    str += '欢迎你!';
+    str += '欢迎你!\n';
+	str += '...........';
     Printer.init(str, {
         selector: 'text_typewriter'
     }).print();
