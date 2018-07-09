47739c2fa999fef8d40f285265bfe9c0ae910c00
bail <yuanquan.ni@qq.com>
2018-07-09 19:58:45 +0800

    ---

--- js/common.js
+++ js/common.js
@@ -6,9 +6,9 @@ $(function () {
     str += 'nice to meet you!\n';
     str += '我是只懒猫~\n';
     str += '有时候也是小白鼠- -\n';
-	str += '你的情谊，我不会拒绝\n';
-	str += '--------(ˇ3ˇ)-------\n';
-	str += '我也愿意陪伴在你身边~\n';
+	str += '\n';
+	str += '\n';
+	str += '\n';
     str += '欢迎你!\n';
 	str += '...........';
     Printer.init(str, {
